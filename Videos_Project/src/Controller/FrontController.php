210d16afba17d467f2762246977420d0db179b09
ThienDuc1112 <95234772+ThienDuc1112@users.noreply.github.com>
2022-06-24 22:07:53 +0700

    Add Asset folders and create controllers

--- Videos_Project/src/Controller/FrontController.php
+++ Videos_Project/src/Controller/FrontController.php
@@ -9,12 +9,26 @@ use Symfony\Component\Routing\Annotation\Route;
 class FrontController extends AbstractController
 {
     /**
-     * @Route("/front", name="app_front")
+     * @Route("/", name="main_page")
      */
-    public function index(): Response
+    public function index()
     {
-        return $this->render('front/index.html.twig', [
-            'controller_name' => 'FrontController',
-        ]);
+        return $this->render('front/index.html.twig');
+    }
+
+    /**
+     * @Route("/video-list", name="video_list")
+     */
+    public function videoList()
+    {
+        return $this->render('front/video_list.html.twig');
+    }
+
+    /**
+     * @Route("/video-details", name="video_details")
+     */
+    public function videoDetails()
+    {
+        return $this->render('front/video_details.html.twig');
     }
 }
